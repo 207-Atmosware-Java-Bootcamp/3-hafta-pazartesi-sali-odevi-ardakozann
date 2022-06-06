62e4fe05581952fa5dcc932cebdedbde6688af2a
ardakozann <ardakozan97@gmail.com>
2022-06-07 01:28:04 +0300

    sicaklik cevirme yapildi.

---
+++ odev2(Sicaklik cevirme)/template.js
@@ -0,0 +1,18 @@
+convertToFahrenheit();
+
+function convertToFahrenheit() {
+    var temperatureValueInCelsius=inputDegree();
+    var temperatureValueInFahrenheit=(temperatureValueInCelsius*1.8)+32;
+    document.write("Celcius Temperature   : "+ temperatureValueInCelsius + '<br>');
+    document.write("Fahrenheit Temperature: "+ temperatureValueInFahrenheit);
+}
+
+function inputDegree() {
+    var temperatureValue;
+    while(true){
+        temperatureValue = Number (prompt("Sıcaklık derecesi giriniz:"));
+        if(!isNaN(temperatureValue)){
+            return temperatureValue;
+        }
+    }
+}
